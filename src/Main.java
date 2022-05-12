3b860bde06b12718c4945658b4d5161e89a9b286
Mikhail Kazannik <kazannik.mihail@gmail.com>
2022-05-12 09:35:09 +0300

    refactor: Спринт 4 Оптимизирован вызов TaskManager

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-import dev.service.InMemoryTaskManager;
-import dev.service.TaskManager;
+import dev.service.Managers;
 import dev.utils.menu.MainMenu;
 
 public class Main {
 
     public static void main(String[] args) {
-        TaskManager manager = new InMemoryTaskManager();
-
         System.out.println("Трекер задач");
-        MainMenu.Menu(manager);
+        MainMenu.Menu(Managers.getDefault());
     }
 }
